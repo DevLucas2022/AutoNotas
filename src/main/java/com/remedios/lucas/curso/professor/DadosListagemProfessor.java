6517eeca49b630d82065c82596e95a3b03158df2
Lucas <lucas.santos517@fatec.sp.gov.br>
2023-10-07 10:41:47 -0300

    :sparkles: update controller

--- src/main/java/com/remedios/lucas/curso/professor/DadosListagemProfessor.java
+++ src/main/java/com/remedios/lucas/curso/professor/DadosListagemProfessor.java
@@ -1,5 +1,7 @@
 package com.remedios.lucas.curso.professor;
 
+import com.remedios.lucas.curso.aluno.Aluno;
+
 import java.time.LocalDate;
 
 public record DadosListagemProfessor(Long id, String nome, LocalDate dataNascimento, String telefone, String email) {
@@ -7,4 +9,5 @@ public record DadosListagemProfessor(Long id, String nome, LocalDate dataNascime
     public DadosListagemProfessor(Professor professor){
         this(professor.getId(), professor.getNome(),professor.getDataNascimento(), professor.getTelefone(), professor.getEmail());
     }
+
 }
