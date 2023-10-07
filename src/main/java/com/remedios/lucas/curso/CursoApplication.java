61114fd43e39a6af064b1809c486365afb6e8497
lucas santos <lucas.santos517@fatec.sp.gov.br>
2023-10-07 15:08:32 -0300

    :sparkles: exclude Security Configuration

--- src/main/java/com/remedios/lucas/curso/CursoApplication.java
+++ src/main/java/com/remedios/lucas/curso/CursoApplication.java
@@ -2,9 +2,11 @@ package com.remedios.lucas.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class CursoApplication {
 
 	public static void main(String[] args) {
