7c9ae5da77cf4a1cd7a1ba8e2937a36247ef6e6a
lucas santos <lucas.santos517@fatec.sp.gov.br>
2023-10-21 15:09:55 -0300

    :sparkles: finished crud disciplina

---
+++ src/main/java/com/remedios/lucas/curso/disciplinas/DisciplinaService.java
@@ -0,0 +1,30 @@
+package com.remedios.lucas.curso.disciplinas;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.remedios.lucas.curso.Endereco.Endereco;
+import com.remedios.lucas.curso.professor.Professor;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DisciplinaService {
+    RestTemplate
+            Template = new RestTemplate();
+    public static Professor consultarProfessor(Long id) throws IOException {
+        URL url = new URL("http://localhost:8080/professores/" + id);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 200) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(connection.getInputStream(), Professor.class);
+        }
+
+        return null;
+    }
+}
